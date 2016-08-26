1495f45466717662fd43ed51edbce8c9f22e9c7b
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-08-26 15:55:58 +0100

    added image support

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,9 @@ class Restaurant < ApplicationRecord
     -> { extending WithUserAssociationExtension },
     dependent: :destroy
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, length: { minimum: 3 }, uniqueness: true
 
   def average_rating
